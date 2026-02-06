d25cac3e0b45e3bfab6a11cf6a0a8dda0f289e6f
Derick Payne <rizonetech@gmail.com>
2026-02-06 14:12:20 +0200

    Use settings.svg icon and embed ExoSuite.ico as app icon

--- build.rs
+++ build.rs
@@ -1,3 +1,12 @@
 fn main() {
+    // Compile Slint UI
     slint_build::compile("ui/main.slint").unwrap();
+    
+    // Embed Windows application icon
+    #[cfg(windows)]
+    {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("resources/ExoSuite.ico");
+        res.compile().unwrap();
+    }
 }
